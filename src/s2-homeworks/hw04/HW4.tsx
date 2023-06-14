8bfc6a3a482b29ef40653f98a94c34736178c179
Andrey_Andrievskikh <sn0abg0@gmail.com>
2023-06-14 21:41:28 +0500

    chore: complete scripts HW4

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import s2 from '../../s1-main/App.module.css'
-import Stand from './Stand'
+import { Stand } from './Stand'
 
 /*
 * 1 - понять (и простить) SuperInputText
@@ -11,10 +11,10 @@ import Stand from './Stand'
 
 export const HW4 = () => {
   return (
-    <div id={'hw4'}>
-      <div className={s2.hwTitle}>Homework #4</div>
-      {/*демонстрация возможностей компонент:*/}
-      <div className={s2.hw}>
+    <div id={ 'hw4' }>
+      <div className={ s2.hwTitle }>Homework #4</div>
+      {/*демонстрация возможностей компонент:*/ }
+      <div className={ s2.hw }>
         <Stand />
       </div>
     </div>
